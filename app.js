8e696998087f4f8baf6f78949dae8d263025f545
Ken Holston <git.kholston@gmail.com>
2020-10-29 15:34:44 -0400

    setup for production

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var inventoryRouter = require('./routes/inventory');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
+app.use(helmet.contentSecurityPolicy({
+  directives:{
+    defaultSrc:["'self'"],
+    scriptSrc:["'self'",'code.jquery.com','cdn.jsdelivr.net'],
+    styleSrc:["'self'", 'cdn.jsdelivr.net']
+  }
+}));
 
 //setup mongoose connection
 var mongoose = require('mongoose');
@@ -26,6 +35,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
